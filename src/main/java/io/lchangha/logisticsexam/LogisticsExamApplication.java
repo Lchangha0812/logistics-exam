9293004e747cad47bb65407e8f03b7965ac5efeb
changha <yullraes@gmail.com>
2025-10-30 23:58:51 +0900

    git init

---
+++ src/main/java/io/lchangha/logisticsexam/LogisticsExamApplication.java
@@ -0,0 +1,13 @@
+package io.lchangha.logisticsexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lchangha/logisticsexam/LogisticsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lchangha.logisticsexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
